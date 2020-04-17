702d8cef09ffd1947114650a02c86eb36a4e50cf
José Mauro Neto <jmauroneto@gmail.com>
2020-04-17 15:10:14 -0300

    Enviando mensagens

--- src/components/Mensageiro/Mensageiro.js
+++ src/components/Mensageiro/Mensageiro.js
@@ -3,33 +3,69 @@ import './Mensageiro.css'
 
 class Mensageiro extends React.Component {
     state={
+        mensagens: [],
         valorUsuario: "",
         valorMensagem: ""
     }
 
+    onChangeUsuario = (event) => {
+		this.setState({
+			valorUsuario: event.target.value
+		})
+	}
+
+    onChangeMensagem = (event) => {
+		this.setState({
+			valorMensagem: event.target.value
+		})
+    }
+
+    enviar = () => {
+        const listaDeMensagens = this.state.mensagens;
+        const mensagem = {
+            usuario: this.state.valorUsuario,
+            texto: this.state.valorMensagem
+        }
+        listaDeMensagens.push(mensagem)
+        this.setState({
+            mensagens: listaDeMensagens, 
+            valorMensagem: ''
+        })
+    }
+
+    atualizaMensagens = () => {
+        const listaDeMensagens = this.state.mensagens.map(mensagem =>{
+                return <div><h4>{mensagem.usuario}: </h4><p>{mensagem.texto}</p></div>
+            
+        })
+        return listaDeMensagens;
+    }
+    
     render(){
-    return (
-        <div className="container">
-            <div className="container-mensagens">
+        const listaAtualizada = this.atualizaMensagens();
+        return (
+            <div className="container">
+                <div className="container-mensagens">
+                    {listaAtualizada}
+                </div>
+                <div className="grid-input">
+                    <input
+                        className={'input-usuario'}
+                        placeholder={'Usuário'}
+                        value={this.state.valorUsuario}
+                        onChange={this.onChangeUsuario}
+                    />
+                    <input
+                        className={'input-mensagem'}
+                        placeholder={'Mensagem'}
+                        value={this.state.valorMensagem}
+                        onChange={this.onChangeMensagem}
+                        onKeyDown={this.enviaComEnter}
+                    />
+                    <button onClick={this.enviar}>Enviar</button>
+                </div>
             </div>
-            <div className="grid-input">
-                <input
-                    className={'input-usuario'}
-                    placeholder={'Usuário'}
-                    value={this.state.valorUsuario}
-                    onChange={this.onChangeUsuario}
-                />
-                <input
-                    className={'input-mensagem'}
-                    placeholder={'Mensagem'}
-                    value={this.state.valorMensagem}
-                    onChange={this.onChangeMensagem}
-                    onKeyDown={this.enviaComEnter}
-                />
-                <button onClick={this.enviar}>Enviar</button>
-            </div>
-        </div>
-    )
+        )
     }
 }
 
