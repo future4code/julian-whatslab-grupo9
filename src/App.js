c839e3d3540c6ba84aa248755f9fe5e8d3986d97
José Mauro Neto <jmauroneto@gmail.com>
2020-04-17 11:03:13 -0300

    Adicionando boilerplate base

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import CardGrande from './components/CardGrande/CardGrande';
+import ImagemButton from './components/ImagemButton/ImagemButton';
+
+function App() {
+  return (
+    <div className="App">
+      <CardGrande 
+          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
+          nome="Labenu" 
+          descricao="Formando desenvolvedores para o mercado de trabalho!" 
+        />
+    </div>
+  );
+}
+
+export default App;
